f69c459eb76b06e89684e1b8327c0b5ff66c8653
Krzysztof Herod <krzysiek.herod@gmail.com>
2011-11-09 14:35:49 +0100

    there are projects in rails 2 with some benefits of rails 3

--- lib/loris.rb
+++ lib/loris.rb
@@ -3,5 +3,5 @@ require 'loris/utils.rb'
 require 'loris/base.rb'
 require 'loris/report.rb'
 
-require 'loris/railtie.rb' if defined?(Rails)
+require 'loris/railtie.rb' if defined?(Rails) && Rails::VERSION::MAJOR > 2
 
