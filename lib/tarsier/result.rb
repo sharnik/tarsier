c1ec5df65d683fe1f1d328da052e7f097dd28ce3
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2012-05-17 16:47:11 +0200

    Move result compilation to the Result class.

--- lib/tarsier/result.rb
+++ lib/tarsier/result.rb
@@ -3,10 +3,72 @@ module Tarsier
     attr_accessor :data
 
     def initialize
+      @data = {}
     end
 
     def get_binding
       binding()
     end
+
+    def compile_data
+      groups = test_case_groups(Tarsier.data)
+      groups.sort! {|a, b| b.keys.length <=> a.keys.length }
+      grouped_lines = data_grouped(Tarsier.data, groups)
+      result = {:collection => [], :mode => Tarsier.mode}
+      groups.each_with_index do |group, index|
+        chunk = {}
+        if Tarsier.mode == :line
+          chunk[:line] = Tarsier.arguments[:line_number]
+          chunk[:file] = Tarsier.arguments[:file]
+        else
+          chunk[:files] = grouped_lines[index].map do |file, lines|
+            { :name => file,
+              :code => lines.sort.map {|line| "#{line + 1}: #{Tarsier.code_lines[file][line]}"}
+            }
+          end
+        end
+        chunk[:test_groups] = group
+        result[:collection] << chunk
+      end
+      @data = result
+    end
+
+    private
+
+    def test_case_groups(data)
+      groups = []
+      Tarsier.data.each do |file, lines|
+        lines.each do |line_number, test_cases|
+          if condition(test_cases, file, line_number)
+            groups << test_cases unless groups.index(test_cases)
+          end
+        end
+      end
+      groups
+    end
+
+    def data_grouped(data, groups)
+      grouped_lines = []
+      Tarsier.data.each do |file, lines|
+        lines.each do |line_number, test_cases|
+          group_id = groups.index(test_cases)
+          unless group_id.nil?
+            grouped_lines[group_id] ||= {}
+            grouped_lines[group_id][file] ||= []
+            grouped_lines[group_id][file] << line_number
+          end
+        end
+      end
+      grouped_lines
+    end
+  
+    def condition(test_cases, file, line_number)
+      if Tarsier.mode == :line
+        file == File.expand_path(Tarsier.arguments[:file]) && (line_number + 1) == Tarsier.arguments[:line_number].to_i
+      else
+        test_cases.length > 1
+      end
+    end
+
   end
 end
